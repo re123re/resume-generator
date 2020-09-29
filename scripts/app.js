d94cf4158922dc3deb2eee4ad4ca86f954cea0a5
ruslanen <ruslan68a@gmail.com>
2020-09-29 07:48:50 +0300

    Init commit.

---
+++ scripts/app.js
@@ -0,0 +1,41 @@
+const { jsPDF } = window.jspdf;
+
+function generatePdf() {
+    const doc = new jsPDF();
+    doc.setFont('Roboto-Light');
+
+    doc.setFontSize(22);
+    doc.text("Петух", 10, 10);
+    doc.line(10,20, 200, 20);
+    //doc.save("resume.pdf");
+
+    doc.setFontSize(16);
+    const firstName = document.getElementById("firstName").value
+    const lastName = document.getElementById("lastName").value;
+    const patronymic = document.getElementById("firstName").value;
+
+    doc.text("Имя: " + firstName, 10, 30);
+    doc.text("Фамилия: " + lastName, 10, 40);
+    doc.text("Отчество: " + patronymic, 10, 50);
+
+    const ph = document.getElementById("photo");
+    toBase64(ph.files[0]).then(function (value) {
+        if (value) {
+            doc.addImage(value, 'webp', 100, 25, 100, 100)
+        }
+        doc.output('dataurlnewwindow');
+    }, function (reason) {
+        console.log(reason); // Ошибка!
+    });
+}
+
+const toBase64 = file => new Promise((resolve, reject) => {
+    const reader = new FileReader();
+    if (file) {
+        reader.readAsDataURL(file);
+        reader.onload = () => resolve(reader.result);
+        reader.onerror = error => reject(error);
+    } else {
+        resolve(null);
+    }
+});
