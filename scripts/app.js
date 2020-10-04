c48db0d712d0dd99790755baa1453f681963943d
123re123 <azat333333@yandex.ru>
2020-10-04 11:01:10 +0300

    Добавил обработку оставшихся полей.

--- scripts/app.js
+++ scripts/app.js
@@ -13,12 +13,78 @@ function onGeneratePdf() {
     const firstName = document.getElementById("firstName").value
     const lastName = document.getElementById("lastName").value;
     const patronymic = document.getElementById("firstName").value;
+    const position = document.getElementById("position").value;
+    const email = document.getElementById("email").value;
+    const employment = document.getElementById("employment").value;
+    const schedule = document.getElementById("schedule").value;
+    const post = document.getElementById("post").value;
+    const assignment = document.getElementById("assignment").value;
 
-    doc.text("Имя: " + firstName, 10, 30);
-    doc.text("Фамилия: " + lastName, 10, 40);
-    doc.text("Отчество: " + patronymic, 10, 50);
+    const city = document.getElementById("city").value;
+    const crossing = document.getElementById("crossing").value;
+    const citizenship = document.getElementById("citizenship").value;
+    const gender = document.getElementById("gender").value;
+    const birthdate = document.getElementById("birthdate").value;
+    const maritalStatus = document.getElementById("maritalStatus").value;
+    const flexCheckIndeterminate = document.getElementById("flexCheckIndeterminate").value;
 
-    
+    const institutio = document.getElementById("institutio").value;
+    const faculty = document.getElementById("faculty").value;
+    const specialty = document.getElementById("specialty").value;
+    const finishYear = document.getElementById("finishYear").value;
+    const educationType = document.getElementById("educationType").value;
+
+    const workPeriod = document.getElementById("workPeriod").value;
+    const appointment = document.getElementById("appointment").value;
+    const organizationWork = document.getElementById("organizationWork").value;
+    const obligation = document.getElementById("obligation").value;
+
+    const training = document.getElementById("training").value;
+    const organizationCoach = document.getElementById("organizationCoach").value;
+    const completion = document.getElementById("completion").value;
+    const languages = document.getElementById("languages").value;
+    const drive = document.getElementById("organizationCoach").value;            //???????
+    const skills = document.getElementById("skills").value;
+    const personalQualities = document.getElementById("personalQualities").value;
+
+    doc.text("Имя: " + firstName, 82, 30);
+    doc.text("Фамилия: " + lastName, 82, 40);
+    doc.text("Отчество: " + patronymic, 82, 50);
+    doc.text("Желаемая зарплата: " + position, 10, 60);
+    doc.text("E-mail: " + email, 10, 70);
+    doc.text("Занятость: " + employment, 10, 80);
+    doc.text("График работы: " + schedule, 10, 90);
+    doc.text("Должность: " + post, 10, 100);
+    doc.text("Готовность к командировкам: " + assignment, 10, 110);
+
+    doc.text("Город: " + city, 10, 120);
+    doc.text("Переезд: " + crossing, 10, 130);
+    doc.text("Гражданство: " + citizenship, 10, 130);
+    doc.text("Пол: " + gender, 10, 140);
+    doc.text("Дата рождения: " + birthdate, 10, 150);
+    doc.text("Семейное положение: " + maritalStatus, 10, 160);
+    doc.text("Есть дети: " + flexCheckIndeterminate, 10, 170);
+
+    doc.text("Учебное заведение: " + institutio, 10, 130);
+    doc.text("Факультет: " + faculty, 10, 130);
+    doc.text("Специальность: " + specialty, 10, 130);
+    doc.text("Год окончания: " + finishYear, 10, 130);
+    doc.text("Форма обучения: " + educationType, 10, 130);
+
+    doc.text("Период работы: " + workPeriod, 10, 130);
+    doc.text("Должность: " + organizationWork, 10, 130);
+    doc.text("Организация: " + organization, 10, 130);
+    doc.text("Должностные обязанности и достижения: ", 10, 130);
+    doc.text(obligation, 10, 130);
+
+    doc.text("Название курса: " + training, 10, 130);
+    doc.text("Наименование организации: " + organizationCoach, 10, 130);
+    doc.text("Год окончания: " + completion, 10, 130);
+    doc.text("Знание иностранных языков: " + languages, 10, 130);
+    doc.text("Водительские права (категории): " + drive, 10, 130); //????????
+    doc.text("Ключевые навыки: " + skills, 10, 130);
+    doc.text("Личные качества: ", 10, 130);
+    doc.text(personalQualities, 10, 130);
 
 
     const ph = document.getElementById("photo");
